8227a78462af70626e50a5338a80e605613c4d17
agart001 <113141664+agart001@users.noreply.github.com>
2024-03-22 14:26:31 -0500

    Add project files.

---
+++ PROG455_FinalProject_ExerciseTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG455_FinalProject_ExerciseTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
